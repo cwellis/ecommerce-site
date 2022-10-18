787b652d692d65575d023c89ad5d8558d04be527
cwellis <cwellis2016@gmail.com>
2022-10-17 21:39:34 -0400

    tablet responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import Logo from '../../assets/logo.png'
 import {CgShoppingBag} from 'react-icons/cg'
+import {GoThreeBars} from 'react-icons/go'
 
 const Header = () => {
+
+    const [ShowMenu, setShowMenu] = useState(true)
+
+    const toggleMenu = () => {
+        setShowMenu((ShowMenu)=>!ShowMenu)
+    }
+
     return (
         <div className="container">
             <div className="logo">
@@ -13,15 +21,24 @@ const Header = () => {
 
             <div className="right">
 
-                <div className="menu">
-                    <ul className="menu">
-                        <li>Collections</li>
-                        <li>Brands</li>
-                        <li>New</li>
-                        <li>Sales</li>
-                        <li>ENG</li>
-                    </ul>
+                <div 
+                    className="bars"
+                    onClick={toggleMenu}
+                >
+                    <GoThreeBars />
                 </div>
+
+                <ul 
+                    className="menu"
+                    style={{display: ShowMenu ? 'inherit' : 'none'}}
+                >
+                    <li>Collections</li>
+                    <li>Brands</li>
+                    <li>New</li>
+                    <li>Sales</li>
+                    <li>ENG</li>
+                </ul>
+                
 
                 <input type="text" className='search' placeholder='Search' />
 
