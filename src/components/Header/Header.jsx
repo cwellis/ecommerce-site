251b2d6d947333db21d483b48ef4af017016cc2a
cwellis <cwellis2016@gmail.com>
2022-10-23 23:22:00 -0400

    fixed error with code not properly updating while edited

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="container">
+            <div className="logo">
+                TESTING
+            </div>
+
+            <div className="menu">
+
+            </div>
+
+            <input type="text" className='search' />
+
+            <span>PNG Icon of Cart</span>
+        </div>
+    )
+}
+
+export default Header
